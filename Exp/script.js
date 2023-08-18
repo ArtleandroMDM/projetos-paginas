69dad03d9f550186f156ec56a049cd50eab9cf00
ArtleandroMDM <138249836+ArtleandroMDM@users.noreply.github.com>
2023-08-18 15:44:48 -0300

    atividade calculadora

---
+++ Exp/script.js
@@ -0,0 +1,35 @@
+
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Erro";
+    }
+}
+
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById("display").value);
+        document.getElementById("display").value = result;
+    } catch (error) {
+        document.getElementById("display").value = "Erro";
+    }
+}
